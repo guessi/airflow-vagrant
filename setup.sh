e7db4bd2848396b73adac5542affb81e0f8347f5
guessi <guessi@gmail.com>
2021-05-22 21:43:16 +0800

    upgrade to airflow 1.10.15

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ apt install -y curl wget vim htop fail2ban default-jre-headless
 apt install -y python3 python3-dev python3-pip python3-mysqldb
 apt install -y mariadb-server-core-10.3 mariadb-client-10.3 libmariadb-dev libmariadbclient-dev
 
-pip3 install 'apache-airflow[mysql]==1.10.10'
+pip3 install 'apache-airflow[mysql]==1.10.15'
 
 if [ -z "$(id airflow)" ]; then
   addgroup --gid 9487 airflow
@@ -18,17 +18,17 @@ fi
 mkdir -p /run/airflow
 chown -R airflow:airflow /run/airflow
 
-curl https://raw.githubusercontent.com/apache/airflow/1.10.10/scripts/systemd/airflow -o /etc/default/airflow
+curl https://raw.githubusercontent.com/apache/airflow/1.10.15/scripts/systemd/airflow -o /etc/default/airflow
 sed -i -e 's#sysconfig#default#' /etc/default/airflow
 
-curl https://raw.githubusercontent.com/apache/airflow/1.10.10/scripts/systemd/airflow.conf -o /etc/tmpfiles.d/airflow.conf
+curl https://raw.githubusercontent.com/apache/airflow/1.10.15/scripts/systemd/airflow.conf -o /etc/tmpfiles.d/airflow.conf
 
 mkdir -p /usr/lib/systemd/system
 
-curl https://raw.githubusercontent.com/apache/airflow/1.10.10/scripts/systemd/airflow-webserver.service -o /usr/lib/systemd/system/airflow-webserver.service
+curl https://raw.githubusercontent.com/apache/airflow/1.10.15/scripts/systemd/airflow-webserver.service -o /usr/lib/systemd/system/airflow-webserver.service
 sed -i -e 's#^ExecStart=/bin/airflow#ExecStart=/usr/local/bin/airflow#' -e 's#sysconfig#default#' /usr/lib/systemd/system/airflow-webserver.service
 
-curl https://raw.githubusercontent.com/apache/airflow/1.10.10/scripts/systemd/airflow-scheduler.service -o /usr/lib/systemd/system/airflow-scheduler.service
+curl https://raw.githubusercontent.com/apache/airflow/1.10.15/scripts/systemd/airflow-scheduler.service -o /usr/lib/systemd/system/airflow-scheduler.service
 sed -i -e 's#^ExecStart=/bin/airflow#ExecStart=/usr/local/bin/airflow#' -e 's#sysconfig#default#' /usr/lib/systemd/system/airflow-scheduler.service
 
 # NOTE: need to execute by 'airflow'
@@ -41,6 +41,10 @@ fi
 # we don't need those examples
 sed -i -e '/load_examples/s/True/False/' /home/airflow/airflow/airflow.cfg
 sed -i -e '/load_default_connections/s/True/False/' /home/airflow/airflow/airflow.cfg
+
+# generate random_secret
+RANDOM_SECRET=$(openssl rand -hex 30)
+sed -i -e 's/^secret_key = .*/secret_key = '${RANDOM_SECRET}'/g' /home/airflow/airflow/airflow.cfg
 
 systemctl daemon-reload
 
@@ -56,11 +60,11 @@ pip3 install google-ads googleads
 
 # sepcial rules for "google-cloud-bigquery"
 pip3 install 'google-cloud-bigquery>=1.24.0,<2.0.0'
-pip3 install 'six>=1.14.0,<2.0.0'
+pip3 install 'six>=1.16.0,<2.0.0'
 
 # tabcmd
-wget https://downloads.tableau.com/esdalt/2020.1.2/tableau-tabcmd-2020-1-2_all.deb -O /tmp/tableau-tabcmd-2020-1-2_all.deb
-apt install -y /tmp/tableau-tabcmd-2020-1-2_all.deb
+wget https://downloads.tableau.com/esdalt/2020.1.17/tableau-tabcmd-2020-1-17_all.deb -O /tmp/tableau-tabcmd-2020-1-17_all.deb
+apt install -y /tmp/tableau-tabcmd-2020-1-17_all.deb
 
 su - airflow -c '/opt/tableau/tabcmd/bin/tabcmd --accepteula'
 
